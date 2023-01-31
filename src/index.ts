ca517911d5ae4da6e827eb498d29031eada54886
aldjanaliev <aldjanaliev@gmail.com>
2023-02-01 00:13:58 +0600

    webpack settings

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {someFun} from "./test"
+import {sayHi} from "./test"
+
+someFun(123)
+
+sayHi("hello baby")
---
+++ src/test.ts
@@ -0,0 +1,8 @@
+export function someFun(arg:number):string{
+    console.log(5555)
+    return ''
+}
+
+export function sayHi(arg:string){
+    console.log(arg)
+}
